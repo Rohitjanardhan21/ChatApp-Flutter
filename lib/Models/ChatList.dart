77d4491d4ce7a16ba2dcbfcee27faa2226abfce0
Rohitjanardhan21 <rohitpalathingal@gmail.com>
2024-05-16 23:22:27 +0530

    commit

--- lib/Models/ChatList.dart
+++ lib/Models/ChatList.dart
@@ -1,11 +1,71 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
-class ChatList{
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        brightness: Brightness.dark,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // This is a placeholder for the actual Home screen implementation
+    return Center(
+      child: Text(
+        'Home Screen',
+        style: TextStyle(fontSize: 24),
+      ),
+    );
+  }
+}
+
+class ChatMessage {
+  String message;
+  bool receiver;
+  ChatMessage({this.message, this.receiver});
+}
+
+class ChatList {
   String name;
   String lastMessage;
-  String userimage;
+  String userImage;
   String time;
   String noOfMessage;
 
-  ChatList({this.name, this.lastMessage, this.userimage, this.time, this.noOfMessage});
+  ChatList({this.name, this.lastMessage, this.userImage, this.time, this.noOfMessage});
 }
